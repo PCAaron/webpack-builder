22a327a0cc26079c70f06b49a347bcc03aecc733
PC_Aaron <1021048274@qq.com>
2020-03-08 16:47:41 +0800

    update:travis CI

--- tests/unit/webpack-base.test.js
+++ tests/unit/webpack-base.test.js
@@ -9,8 +9,8 @@ describe('webpack.base.js 测试用例', () =>{
     // console.log(baseConfig)
     it('entry', () => {
         expect(baseConfig.entry.index)
-            .toBe('D:/github/blogCode/webpack/webpack-builder/tests/smoke/template/src/index/index.js')
+            .toContain('webpack-builder/tests/smoke/template/src/index/index.js')
         expect(baseConfig.entry.about)
-            .toBe('D:/github/blogCode/webpack/webpack-builder/tests/smoke/template/src/about/index.js')
+            .toContain('webpack-builder/tests/smoke/template/src/about/index.js')
     })
 })
